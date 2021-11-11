33d6670a046233e59b9a7ba3da134abfed6af5cb
Rachel Cianfichi <cianfich@gmail.com>
2021-11-11 12:01:51 -0500

    Updated service worker to include indexeddb.js file.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const filesToCache = [
     '/styles.css',
     '/manifest.json',
     '/index.js',
+    '/indexeddb.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
